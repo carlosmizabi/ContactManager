2fc5859ac212c51e4d70eaf380b0dab82607d230
kalibuda@hotmail.com <kalibuda@hotmail.com>
2013-02-03 13:42:30 +0000

    Cosmetic change and small logic corrections
    Created and index and corrected the addFutureMeeting logic also added a
    new method to update the date so that it can be used when the current
    date needs to be updated.

--- src/manager/ContactManagerImpl.java
+++ src/manager/ContactManagerImpl.java
@@ -9,7 +9,6 @@ import java.util.List;
 import java.util.Set;
 import java.util.TreeSet;
 
-
 import interfaces.Contact;
 import interfaces.ContactManager;
 import interfaces.Editor;
@@ -17,139 +16,200 @@ import interfaces.FutureMeeting;
 import interfaces.Meeting;
 import interfaces.PastMeeting;
 
+/* - ///////////////////////////////////////////////////////
+ * 
+ * Class INDEX:
+ * 
+ * 1# CLASS FIELDS & CONTRUCTOR/S
+ * 2# ADD (new objects to mgr's lists)
+ * 3# +METHODS (all non-categorized methods)
+ * 4# SETTERS
+ * 5# GETTERS
+ * 
+ * - ///////////////////////////////////////////////// - */
+
 public class ContactManagerImpl implements ContactManager {
 	
-	private Editor editor;
-	Comparator<Contact> comparator;
-	private List<Meeting> meetingList;
-	private List<PastMeeting> pastMeetingsList;
-	private Calendar date;
-	private Set<Contact> contacts;
+	/*
+	 * 1# - CLASS FIELDS & CONTRUCTOR/S		///////////////////////////////////////////
+	 */
 	
-	private int meetingCounter = 0;
-	private int contactCounter = 0;
+	private Editor editor;						// the editor will fetch and store all information
+	Comparator<Contact> comparator;				// needed for contacts TreeSet iteration
+	private List<Meeting> meetingList;			// store all meetings
+	private List<PastMeeting> pastMeetingsList;	// store past meetings
+	private Calendar date;	// current date should be updated ALWAYS before use -> updateMgrDate();
+	private Set<Contact> contacts;				// contact list
+	
+	/* Meetings' and contacts' classes don't manage
+	 * the id assignment and management, hence it needs
+	 * to be managed by the client class
+	 */
+	private int meetingCounter = 0;				// counter for meetings
+	private int contactCounter = 0;				// counter for contacts
 	
 	public ContactManagerImpl()
 	{
-		date = new GregorianCalendar();
+		date = new GregorianCalendar();		// date at initialization			
 		comparator = new ContactComparator();
-		contacts= new TreeSet<Contact>(comparator);
+		contacts = new TreeSet<Contact>(comparator);
 		List<Meeting> meetingList = new LinkedList<Meeting>();
 		List<PastMeeting> pastMeetingsList = new LinkedList<PastMeeting>();
 		editor = new EditorImpl(contacts, meetingList, pastMeetingsList);
-		setCounters();
-	}
-
-	// set the counter to initial list sizes
-	//
-	private void setCounters()
-	{
-		meetingCounter = meetingList.size();
-		contactCounter = contacts.size();
+		setCounters();	// set the counter
 	}
 	
-	private int getContactNewId()
-	{
-		contactCounter++;
-		return contactCounter;
-	}
-	private int getMeetingNewId()
-	{
-		meetingCounter++;
-		return meetingCounter;
-	}
+	
+	/*
+	 * 2# - ADD /////////////////////////////////////////////////////////////////
+	 */
+
 	@Override
-	public int addFutureMeeting(Set<Contact> contacts, Calendar date) 
+	public void addNewPastMeeting(Set<Contact> contacts, Calendar date, String text) 
 	{
-		if(date.compareTo(this.date) > 0) {
-			throw new IllegalArgumentException("Date is in the pasty!"); 
-		}else if(!contacts.containsAll(contacts)) {
-			throw new IllegalArgumentException("Cannot Add contact list with unknown Contacts!" +
-					"\n" + "Please add them first");
-		}else{	
 		MeetingImpl meeting = new MeetingImpl(contacts, date, getMeetingNewId());
 		meetingList.add(meeting);
-		return this.meetingCounter;
-		}
 	}
 
 	@Override
-	public PastMeeting getPastMeeting(int id) {
+	public void addMeetingNotes(int id, String text) 
+	{
 		// TODO Auto-generated method stub
-		return null;
 	}
 
 	@Override
-	public FutureMeeting getFutureMeeting(int id) {
-		
-		return null;
-	}
-
-	@Override
-	public Meeting getMeeting(int id) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public List<Meeting> getFutureMeetingList(Contact contact) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public List<Meeting> getFutureMeetingList(Calendar date) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public List<PastMeeting> getPastMeetingList(Contact contact) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public void addNewPastMeeting(Set<Contact> contacts, Calendar date,
-			String text) {
-		MeetingImpl meeting = new MeetingImpl(contacts, date, getMeetingNewId());
-		meetingList.add(meeting);
-
-	}
-
-	@Override
-	public void addMeetingNotes(int id, String text) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void addNewContact(String name, String notes) {
+	public void addNewContact(String name, String notes) 
+	{
 		if(name == null | notes == null) {
 			throw new NullPointerException("Date is in the pasty!"); 
 		}else{
 			ContactImpl contact = new ContactImpl(name, notes, getContactNewId());
 			contacts.add(contact);
 		}
-		
 	}
+	
+	@Override
+	public int addFutureMeeting(Set<Contact> contactSet, Calendar meetingDate) 
+	/* 
+	 *  take the date of the meeting and compare with current date
+	 */
+	{
+		updateMgrDate();
+		if(meetingDate.compareTo(this.date) < 0) // e.g., 2012 - 2013 = -1
+		{
+			throw new IllegalArgumentException("Date is in the past!"); 
+			
+		}else if(!this.contacts.containsAll(contactSet)) 
+			//
+			// Do all contacts on the new meeting's set already exist in Mgr's contact list?
+		{
+			throw new IllegalArgumentException("Cannot Add contact list with unknown Contacts!" +
+					"\n" + "Please add them first");
+		}else
+		{	
+			Meeting newMeeting = new MeetingImpl(contactSet, meetingDate, getMeetingNewId());
+			meetingList.add(newMeeting);
+			return this.meetingCounter;
+		}
+	} // close addFutureMeeting(...); //
+	
+	/*
+	 * 3# - +METHODS /////////////////////////////////////////////////////////////////
+	 */
+	
+	/*
+	 * Update the date to current time when using date.
+	 * It is always good to update the current date.
+	 * The initialized date at construction might have
+	 * became out dated during use.
+	 */
+	private void updateMgrDate() { this.date = new GregorianCalendar(); }
+	
 
 	@Override
-	public Set<Contact> getContacts(int... ids) {
+	public void flush() { editor.save(contacts, meetingList); }
+
+	/*
+	 * 4# - SETTERS /////////////////////////////////////////////////////////////////
+	 */
+
+	/* set the counter to initial list sizes
+	 * fetch the lists present size
+	 */
+	private void setCounters()
+	{
+		meetingCounter = meetingList.size();
+		contactCounter = contacts.size();
+	}
+	
+	/*
+	 * 5# - GETTERS /////////////////////////////////////////////////////////////////
+	 */
+	
+	private int getContactNewId()
+	{
+		contactCounter++;	// increment contacts' counter
+		return contactCounter;
+	}
+	private int getMeetingNewId()
+	{
+		meetingCounter++;  // increment meetings' counter
+		return meetingCounter;
+	}
+	
+	@Override
+	public PastMeeting getPastMeeting(int id) 
+	{
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	public Set<Contact> getContacts(String name) {
+	public FutureMeeting getFutureMeeting(int id) 
+	{
+		return null;
+	}
+
+	@Override
+	public Meeting getMeeting(int id) 
+	{
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	public void flush() {
-		// TODO 
-		editor.save(contacts, meetingList);
+	public List<Meeting> getFutureMeetingList(Contact contact) 
+	{
+		// TODO Auto-generated method stub
+		return null;
 	}
 
-}
+	@Override
+	public List<Meeting> getFutureMeetingList(Calendar date) 
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<PastMeeting> getPastMeetingList(Contact contact) 
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Set<Contact> getContacts(int... ids) 
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Set<Contact> getContacts(String name) 
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+} // close class ContactManagerImpl{} //
