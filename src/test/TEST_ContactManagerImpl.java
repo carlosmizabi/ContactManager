e59632e5415341af4b9417fc6c3d2f33001474f6
kalibuda@hotmail.com <kalibuda@hotmail.com>
2013-02-04 23:18:40 +0000

    Building the ContactManagerImpl tests : addFutureMeeting

--- src/test/TEST_ContactManagerImpl.java
+++ src/test/TEST_ContactManagerImpl.java
@@ -2,6 +2,19 @@ package test;
 
 import static org.junit.Assert.*;
 
+import java.util.Calendar;
+import java.util.Comparator;
+import java.util.GregorianCalendar;
+import java.util.Set;
+import java.util.TreeSet;
+
+import interfaces.Contact;
+import interfaces.ContactManager;
+
+import manager.ContactComparator;
+import manager.ContactImpl;
+import manager.ContactManagerImpl;
+
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -9,6 +22,9 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class TEST_ContactManagerImpl {
+	
+	ContactManager cmgr;
+	Comparator<Contact> comparator;
 
 //	@BeforeClass
 //	public static void setUpBeforeClass() throws Exception {
@@ -20,15 +36,35 @@ public class TEST_ContactManagerImpl {
 
 	@Before
 	public void setUp() throws Exception {
+		cmgr = new ContactManagerImpl();
+		cmgr.addNewContact("Primero Contato", "It's the first guy.");
+		cmgr.addNewContact("Segundo Contato", "It's the second guy.");
+		
 	}
 
-//	@After
-//	public void tearDown() throws Exception {
-//	}
+	@After
+	public void tearDown() throws Exception {
+		cmgr = null;
+	}
 
 	@Test
 	public void testAddFutureMeeting() {
-		fail("Not yet implemented");
+		Calendar date = new GregorianCalendar(2014, 01, 01);
+		comparator = new ContactComparator();
+		Set<Contact> contactSet = new TreeSet<Contact>(comparator);
+		String james = "James T. Kirk";
+		String jean = "Jean-Luc Picard";
+		cmgr.addNewContact(james, "Spok");
+		cmgr.addNewContact(jean, "Data");
+		
+		for(Contact contact : cmgr.getContacts(james))
+		{ if(contact.getName().equals(james)) { contactSet.add(contact); } }
+		
+		for(Contact contact : cmgr.getContacts(jean))
+		{ if(contact.getName().equals(jean)) { contactSet.add(contact); } }
+		
+		
+		cmgr.addFutureMeeting(contactSet, date);
 	}
 
 	@Test
